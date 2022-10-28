b58af041a2473bcfda7983d8ce59a3ecfc78c09e
Jordan <jordangottardo@libero.it>
2022-10-28 09:46:20 +0200

    Activate tokens (#1)
    * add subscribe user
    
    * update version

--- TooGoodToGoNotifierAndroidApp/TooGoodToGoNotifierAndroidApp/ProductsClient.cs
+++ TooGoodToGoNotifierAndroidApp/TooGoodToGoNotifierAndroidApp/ProductsClient.cs
@@ -23,21 +23,46 @@ namespace TooGoodToGoNotifierAndroidApp
             _client = new HttpClient();
         }
 
+        public async Task SubscribeUser()
+        {
+            try
+            {
+                var uri = await GetUsersUrl();
+
+                var response = await _client.PostAsync(uri, null);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    Log.Error(Constants.AppName, $"An error occurred while subscribing user StatusCode: {response.StatusCode} ReasonPhrase: {response.ReasonPhrase}");
+
+                    return;
+                }
+            
+                var content = await response.Content.ReadAsStringAsync();
+                var products = JsonSerializer.Deserialize<List<ProductDto>>(content);
+
+            }
+            catch (Exception e)
+            {
+                Log.Error(Constants.AppName, $"An error occurred while retrieving products {e}");
+            }
+        }
+
         public async Task<IEnumerable<ProductDto>> GetAvailableProducts()
         {
             try
             {
-                var uri = await GetUrl();
+                var uri = await GetProductsUrl();
 
                 var response = await _client.GetAsync(uri);
 
                 if (!response.IsSuccessStatusCode)
                 {
                     Log.Error(Constants.AppName, $"An error occurred while retrieving available products StatusCode: {response.StatusCode} ReasonPhrase: {response.ReasonPhrase}");
-                
+
                     return Enumerable.Empty<ProductDto>();
                 }
-            
+
                 var content = await response.Content.ReadAsStringAsync();
                 var products = JsonSerializer.Deserialize<List<ProductDto>>(content);
 
@@ -50,7 +75,23 @@ namespace TooGoodToGoNotifierAndroidApp
             }
         }
 
-        private static async Task<Uri> GetUrl()
+        private static async Task<Uri> GetUsersUrl()
+        {
+            var serverUrl = await SecureStorage.GetAsync(ServerUrlKey);
+            var username = await SecureStorage.GetAsync(UsernameKey);
+
+            var uri = new UriBuilder(serverUrl)
+            {
+                Path = "tokens/update",
+                Query = $"userEmail={username}"
+            }.Uri;
+
+            Log.Info(Constants.AppName, $"uri={uri}");
+
+            return uri;
+        }
+
+        private static async Task<Uri> GetProductsUrl()
         {
             var serverUrl = await SecureStorage.GetAsync(ServerUrlKey);
             var username = await SecureStorage.GetAsync(UsernameKey);
